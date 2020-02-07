5ef9824ef8ecdbf30d58f51de6f9772369ae9e07
carlosariasf <carlosarias@icreativa.com.co>
2020-02-07 17:30:52 +0000

    [add] check shape

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    " Shape of matrix "
+    if not isinstance(matrix, list):
+        return []
+    return [len(matrix)] + matrix_shape(matrix[0])
