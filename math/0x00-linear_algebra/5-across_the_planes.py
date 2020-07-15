d352affd92631f291848db8b502339bde9781ab5
carlosariasf <carlosarias@icreativa.com.co>
2020-07-15 23:56:03 +0000

    [fix] same shape check

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,8 +4,9 @@
 
 def add_matrices2D(mat1, mat2):
     " Across The Planes "
-    if len(mat1[0]) != len(mat2[0]):
-        return None
+    for i in range(len(mat1)):
+        if len(mat1[i]) != len(mat2[i]):
+            return None
     arr = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1))]
            for i in range(len(mat1[0]))]
     return arr
