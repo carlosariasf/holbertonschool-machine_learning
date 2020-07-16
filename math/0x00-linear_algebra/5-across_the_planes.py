ca84c9ac0faaf2f3a6af85a43d5adfda411c1b60
carlosariasf <carlosarias@icreativa.com.co>
2020-07-16 00:05:37 +0000

    [fix] order sub

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,9 +4,8 @@
 
 def add_matrices2D(mat1, mat2):
     " Across The Planes "
-    for i in range(len(mat1)):
-        if len(mat1[i]) != len(mat2[i]):
-            return None
-    arr = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1))]
-           for i in range(len(mat1[0]))]
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    arr = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+           for i in range(len(mat1))]
     return arr
