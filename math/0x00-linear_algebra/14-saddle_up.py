d3a11e075e2804d2a5d0e9a30afa71cb74ea0782
carlosariasf <carlosarias@icreativa.com.co>
2020-07-16 19:30:44 +0000

    [fix] impot numpy to matrix multiplication

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 " Flip me over "
 
+import numpy as np
+
 
 def np_matmul(mat1, mat2):
     "  def np_matmul(mat1, mat2)  "
-    return mat1 @ mat2
+    return np.dot(mat1, mat2)
