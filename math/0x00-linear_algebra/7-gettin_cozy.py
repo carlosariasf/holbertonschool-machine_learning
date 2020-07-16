6f1507e4a251ee0b79935f0ca18b054fae5745dc
carlosariasf <carlosarias@icreativa.com.co>
2020-07-16 04:13:02 +0000

    [fix] control errors

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -4,7 +4,7 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     " Howdy Partner "
-    if len(mat1[0]) == len(mat2[0]) or len(mat1) == len(mat2) or axis <= 1:
+    if (len(mat1[0]) == len(mat2[0]) or len(mat1) == len(mat2)) and axis <= 1:
         if axis == 0:
             arr = mat1 + mat2
         elif axis == 1:
