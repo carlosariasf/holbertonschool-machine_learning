7465b96bbe5bd450cd51cc9a0ed982e93eedf8da
carlosariasf <carlosarias@icreativa.com.co>
2020-07-17 04:37:55 +0000

    [fix] 2d concat

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -7,7 +7,10 @@ def cat_matrices(mat1, mat2, axis=0):
     shape1, shape2 = recur(mat1), recur(mat2)
     if len(shape1) != len(shape2) and shape1 != shape2:
         return None
-    arr = mat1 + mat2
+    if axis == 0:
+        arr = mat1 + mat2
+    else:
+        arr = cat_matrices2D(mat1, mat2, axis)
     return arr
 
 
@@ -16,3 +19,21 @@ def recur(matrix):
     if not isinstance(matrix, list):
         return []
     return [len(matrix)] + recur(matrix[0])
+
+
+def matrix_transpose(matrix):
+    " Shape of matrix "
+    arr = [[matrix[j][i] for j in range(len(matrix))]
+           for i in range(len(matrix[0]))]
+    return arr
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    " Howdy Partner "
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        arr = mat1 + mat2
+    elif axis == 1 and len(mat1) == len(mat2):
+        arr = [x+y for x, y in zip(mat1, mat2)]
+    else:
+        return None
+    return arr
