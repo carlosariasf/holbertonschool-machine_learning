e541349373547e8980dea76ab6fa593aea14ee18
carlosariasf <carlosarias@icreativa.com.co>
2020-07-17 01:19:39 +0000

    [add] unfinished matrix sum

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+" Flip me over "
+
+
+def add_matrices(mat1, mat2):
+    " The Whole Barn  "
+    if recur(mat1) != recur(mat2):
+        return None
+    if len(recur(mat1)) > 1:
+        arr = [map(sum, zip(*t)) for t in zip(mat1, mat2)]
+        return arr
+    else:
+        arr = [mat1[i] + mat2[i] for i in range(len(mat1))]
+    return arr
+
+
+def recur(matrix):
+    if not isinstance(matrix, list):
+        return []
+    return [len(matrix)] + recur(matrix[0])
