dab027f2bd85d02c72d90667b11e4067b52f9e14
carlosariasf <carlosarias@icreativa.com.co>
2020-07-27 01:10:13 +0000

    [fix] type number error

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,5 +6,10 @@ def poly_integral(poly, C=0):
     ''' integral function '''
     if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
         return None
-    intl = [C] + [poly[x]/(x + 1) for x in range(len(poly))]
+    intl = [C] + list(map(type_n, [poly[x]/(x + 1) for x in range(len(poly))]))
     return intl
+
+
+def type_n(x):
+    ''' type'''
+    return int(x) if x.is_integer() else x
